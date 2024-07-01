78f9baa82fc53281e3fc7ebb8ee5b5df5f28a6ac
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-01 14:41:06 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -19,4 +19,32 @@ export const fetchPopularWords = async () => {
         const wordData = await searchWord(word, false);
         WordStore.update(s => { s.popularWords = [ ...s.popularWords, wordData[0] ]});
     });
+import { setupIonicReact } from '@ionic/react';
+import { WordStore } from './store';
+
+// Configure Ionic React settings
+export const configureIonicReact = () => {
+  setupIonicReact({
+    mode: 'ios', // Set the default mode to iOS
+    rippleEffect: false, // Disable ripple effects globally
+    // Add more global configuration options as needed
+  });
 }
+
+export const searchWord = async (word, returnOne = true) => {
+  const response = await fetch(`${API_URL}${word.toLowerCase()}`);
+  const data = await response.json();
+  return returnOne ? data[0] : data;
+}
+
+export const fetchPopularWords = async () => {
+  const words = ["mobile", "applications", "ionic", "framework"];
+
+  words.forEach(async word => {
+    const wordData = await searchWord(word, false);
+    WordStore.update(s => { s.popularWords = [...s.popularWords, wordData[0]] });
+  });
+}
+
+// Call the configureIonicReact function to set up Ionic React
+configureIonicReact();
