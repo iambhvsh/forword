ee514b2ffd1f34c321a160693d947ca18c9e4605
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-01 16:02:37 +0530

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,22 @@
+import { WordStore } from "./store";
+
+const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
 export const searchWord = async (word, returnOne = true) => {
-  const API_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-  const response = await fetch(`${API_URL}${word.toLowerCase()}`);
-  const data = await response.json();
-  return returnOne ? data[0] : data;
+
+    const response = await fetch(`${ API_URL }${ word.toLowerCase() }`);
+    const data = await response.json();
+
+    return returnOne ? data[0] : data;
 }
 
 export const fetchPopularWords = async () => {
-  const words = ["mobile", "applications", "ionic", "framework"];
 
-  words.forEach(async word => {
-    const wordData = await searchWord(word, false);
-    WordStore.update(s => { s.popularWords = [...s.popularWords, wordData[0]] });
-  });
+    const words = ["mobile", "applications", "ionic", "framework"];
+
+    words.forEach(async word => {
+
+        const wordData = await searchWord(word, false);
+        WordStore.update(s => { s.popularWords = [ ...s.popularWords, wordData[0] ]});
+    });
 }
