38d0586d162e0f6f98810e40988a21cca2bf2494
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-01 17:46:41 +0530

    Update NoSearch.jsx

--- src/components/NoSearch.jsx
+++ src/components/NoSearch.jsx
@@ -1,12 +1,19 @@
 import { IonCol, IonLabel, IonRow } from "@ionic/react";
 
 export const NoSearch = () => (
-    <IonRow className="ion-text-center ion-align-items-center ion-margin-top">
-        <IonCol size="10">
-            <IonLabel>
-                <h2>Search for a word in the English language</h2>
-                <p>This app will give you word meanings, phonetics, origin, and also an audio clip so you can hear what it sounds like.</p>
-            </IonLabel>
-        </IonCol>
-    </IonRow>
+    <div style={{
+        height: '100vh',  // Set height of the container to full viewport height
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }}>
+        <IonRow className="ion-text-center">
+            <IonCol size="10">
+                <IonLabel>
+                    <h2>Search for a word in the English language</h2>
+                    <p>This app will give you word meanings, phonetics, origin, and also an audio clip so you can hear what it sounds like.</p>
+                </IonLabel>
+            </IonCol>
+        </IonRow>
+    </div>
 );
