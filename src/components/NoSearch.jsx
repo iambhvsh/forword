fc9b5ebb3fb0b2c0100d09b3b1d5d7c023d250ed
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-01 17:39:53 +0530

    Update NoSearch.jsx

--- src/components/NoSearch.jsx
+++ src/components/NoSearch.jsx
@@ -1,13 +1,11 @@
 import { IonCol, IonLabel, IonRow } from "@ionic/react";
 
 export const NoSearch = () => (
-
-    <IonRow className="ion-text-center ion-justify-content-center ion-margin-top">
+    <IonRow className="ion-text-center ion-align-items-center ion-margin-top">
         <IonCol size="10">
             <IonLabel>
                 <h2>Search for a word in the English language</h2>
-                <p>This app will give you word meaninigs, phonetics, origin and also an audio clip so you can hear what it sounds like.</p>
-                <lottie-player src="https://assets7.lottiefiles.com/packages/lf20_n2m0isqh.json" mode="bounce" background="transparent" speed="0.8" loop autoplay></lottie-player>
+                <p>This app will give you word meanings, phonetics, origin, and also an audio clip so you can hear what it sounds like.</p>
             </IonLabel>
         </IonCol>
     </IonRow>
